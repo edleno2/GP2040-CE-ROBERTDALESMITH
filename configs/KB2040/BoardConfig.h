32c8ae688301032c34282dfbf42120ba37540fa3
Robert Dale Smith <robert@robertdalesmith.com>
2023-05-03 16:35:35 -0500

    updates kb2040 board config for v0.7.0

--- configs/KB2040/BoardConfig.h
+++ configs/KB2040/BoardConfig.h
@@ -28,17 +28,10 @@
 #define PIN_BUTTON_R2   -1          // R2 / RT / ZR / R2 / 8 / K3
 #define PIN_BUTTON_L2   29          // L2 / LT / ZL / L2 / 7 / K4
 #define PIN_BUTTON_B3   5           // B3 / X / Y / Square / 1 / P1
-<<<<<<< HEAD
 #define PIN_BUTTON_B4   2           // B4 / Y / X / Triangle / 4 / P2
 #define PIN_BUTTON_R1   -1          // R1 / RB / R / R1 / 6 / P3
 #define PIN_BUTTON_L1   28          // L1 / LB / L / L1 / 5 / P4
 #define PIN_BUTTON_S1   -1          // S1 / Back / Minus / Select / 9 / Coin
-=======
-#define PIN_BUTTON_B4   4           // B4 / Y / X / Triangle / 4 / P2
-#define PIN_BUTTON_R1   -1          // R1 / RB / R / R1 / 6 / P3
-#define PIN_BUTTON_L1   -1          // L1 / LB / L / L1 / 5 / P4
-#define PIN_BUTTON_S1   20          // S1 / Back / Minus / Select / 9 / Coin
->>>>>>> 3a66268 (feature: updates kb2040 for jslider toggle and updates jslider toggle to support active high)
 #define PIN_BUTTON_S2   18          // S2 / Start / Plus / Start / 10 / Start
 #define PIN_BUTTON_L3   -1          // L3 / LS / LS / L3 / 11 / LS
 #define PIN_BUTTON_R3   -1          // R3 / RS / RS / R3 / 12 / RS
@@ -46,15 +39,10 @@
 #define PIN_BUTTON_A2   -1          // A2 / ~ / Capture / ~ / 14 / ~
 #define PIN_BUTTON_TURBO -1         // Turbo
 #define PIN_BUTTON_REVERSE -1       // UDLR Reverse
-<<<<<<< HEAD
 #define PIN_SLIDER_LS   -1          // Left Stick Slider
 #define PIN_SLIDER_RS    6          // Right Stick Slider
 #define PIN_SLIDER_SOCD_ONE    -1   // SOCD Slider Pin One
 #define PIN_SLIDER_SOCD_TWO    -1   // SOCD Slider Pin Two
-=======
-#define PIN_SLIDER_LS    6          // Left Stick Slider
-#define PIN_SLIDER_RS    7          // Right Stick Slider
->>>>>>> 3a66268 (feature: updates kb2040 for jslider toggle and updates jslider toggle to support active high)
 
 // This is the SOCD section.
 // SOCD stands for `simultaneous opposing cardinal directions`.
@@ -62,8 +50,16 @@
 // 1 - `SOCD_MODE_NEUTRAL` - This is a neutral SOCD.  EG. when you press `up` + `down` no input will be registered.
 // 2 - `SOCD_MODE_UP_PRIORITY` - This is up priority SOCD.  EG. when you press `up` + `down` `up` will be registered.
 // 3 - `SOCD_MODE_SECOND_INPUT_PRIORITY` - This is last priority SOCD.  EG. when you press and hold `up` then press `down` `down` will be registered.
+// 4 - `SOCD_MODE_FIRST_INPUT_PRIORITY` - This is first priority SOCD.  EG. when you press and hold `up` then press `down` `up` will be registered.
 
 #define DEFAULT_SOCD_MODE SOCD_MODE_NEUTRAL
+// SOCD Slider Slot Defaults
+#define SLIDER_SOCD_SLOT_ONE SOCD_MODE_UP_PRIORITY
+#define SLIDER_SOCD_SLOT_TWO  SOCD_MODE_SECOND_INPUT_PRIORITY
+#define SLIDER_SOCD_SLOT_DEFAULT SOCD_MODE_NEUTRAL
+
+#define DEFAULT_INPUT_MODE INPUT_MODE_XINPUT //INPUT_MODE_XINPUT (XInput), INPUT_MODE_SWITCH (Nintendo Switch), INPUT_MODE_HID (D-Input), INPUT_MODE_KEYBOARD (Keyboard)
+#define DEFAULT_DPAD_MODE DPAD_MODE_DIGITAL  //DPAD_MODE_DIGITAL, DPAD_MODE_LEFT_ANALOG, DPAD_MODE_RIGHT_ANALOG, 
 
 // This is the LEDs section.
 // The default `TURBO_LED_PIN` pin is set to `15` ( it is recommended to run through 3V3(OUT) with a resistor)
@@ -87,8 +83,7 @@
 // The default LEDS_[BUTTON] is an order and has nothing to do with what GPIO pin something is connected to.
 // Unless you are planning on running custom animations I would recommmend you leave this as is.
 
-#define TURBO_ENABLED 0
-#define TURBO_LED_PIN 15
+#define TURBO_LED_PIN -1
 
 #define BOARD_LEDS_PIN 17
 
@@ -109,7 +104,6 @@
 #define LEDS_BUTTON_B2   9
 #define LEDS_BUTTON_R2   10
 #define LEDS_BUTTON_L2   11
-
 
 // This is the Player LED section.  
 // In this section you can specify if Player LEDs will be active, and, if active, which pins will be used for them.
@@ -206,11 +200,7 @@
 #define REVERSE_LEFT_DEFAULT 1
 #define REVERSE_RIGHT_DEFAULT 1
 
-<<<<<<< HEAD
-#define JSLIDER_ENABLED 1 // TODO LOOK?!
-=======
 #define JSLIDER_ENABLED 1
->>>>>>> 3a66268 (feature: updates kb2040 for jslider toggle and updates jslider toggle to support active high)
 #define BUTTON_LAYOUT BUTTON_LAYOUT_STICK
 #define BUTTON_LAYOUT_RIGHT BUTTON_LAYOUT_VEWLIX
 #define SPLASH_MODE NOSPLASH
@@ -249,5 +239,47 @@
 // Extra Button Add-on setting
 #define EXTRA_BUTTON_MASK GAMEPAD_MASK_A1 // Sets boot button to Home/Guide button
 #define EXTRA_BUTTON_PIN 11
+
+// Keyboard Mapping Configuration
+// List of HID keycodes can be located here: https://github.com/hathach/tinyusb/blob/3623ba1884ddff23e9b64766cb6dd032f1425846/src/class/hid/hid.h#L356
+// Even for the modifier keys, HID_KEY entries should be used as the implementation expects those and will convert as necessary.
+#define KEY_DPAD_UP     HID_KEY_ARROW_UP      // UP
+#define KEY_DPAD_DOWN   HID_KEY_ARROW_DOWN    // DOWN
+#define KEY_DPAD_RIGHT  HID_KEY_ARROW_RIGHT   // RIGHT
+#define KEY_DPAD_LEFT   HID_KEY_ARROW_LEFT    // LEFT
+#define KEY_BUTTON_B1   HID_KEY_SHIFT_LEFT    // B1 / A / B / Cross / 2 / K1
+#define KEY_BUTTON_B2   HID_KEY_Z             // B2 / B / A / Circle / 3 / K2
+#define KEY_BUTTON_R2   HID_KEY_X             // R2 / RT / ZR / R2 / 8 / K3
+#define KEY_BUTTON_L2   HID_KEY_V             // L2 / LT / ZL / L2 / 7 / K4
+#define KEY_BUTTON_B3   HID_KEY_CONTROL_LEFT  // B3 / X / Y / Square / 1 / P1
+#define KEY_BUTTON_B4   HID_KEY_ALT_LEFT      // B4 / Y / X / Triangle / 4 / P2
+#define KEY_BUTTON_R1   HID_KEY_SPACE         // R1 / RB / R / R1 / 6 / P3
+#define KEY_BUTTON_L1   HID_KEY_C             // L1 / LB / L / L1 / 5 / P4
+#define KEY_BUTTON_S1   HID_KEY_5             // S1 / Back / Minus / Select / 9 / Coin
+#define KEY_BUTTON_S2   HID_KEY_1             // S2 / Start / Plus / Start / 10 / Start
+#define KEY_BUTTON_L3   HID_KEY_EQUAL         // L3 / LS / LS / L3 / 11 / LS
+#define KEY_BUTTON_R3   HID_KEY_MINUS         // R3 / RS / RS / R3 / 12 / RS
+#define KEY_BUTTON_A1   HID_KEY_9             // A1 / Guide / Home / PS / 13 / ~
+#define KEY_BUTTON_A2   HID_KEY_F2            // A2 / ~ / Capture / ~ / 14 / ~
+
+// Hotkey Action Mapping
+// Find the list of hotkey actions in GamepadEnums.h
+#define HOTKEY_F1_UP_MASK      GAMEPAD_MASK_UP
+#define HOTKEY_F1_UP_ACTION    HOTKEY_HOME_BUTTON
+#define HOTKEY_F1_DOWN_MASK    GAMEPAD_MASK_DOWN
+#define HOTKEY_F1_DOWN_ACTION  HOTKEY_DPAD_DIGITAL
+#define HOTKEY_F1_LEFT_MASK    GAMEPAD_MASK_LEFT
+#define HOTKEY_F1_LEFT_ACTION  HOTKEY_DPAD_LEFT_ANALOG
+#define HOTKEY_F1_RIGHT_MASK   GAMEPAD_MASK_RIGHT
+#define HOTKEY_F1_RIGHT_ACTION HOTKEY_DPAD_RIGHT_ANALOG
+
+#define HOTKEY_F2_UP_MASK      GAMEPAD_MASK_UP
+#define HOTKEY_F2_UP_ACTION    HOTKEY_SOCD_UP_PRIORITY
+#define HOTKEY_F2_DOWN_MASK    GAMEPAD_MASK_DOWN
+#define HOTKEY_F2_DOWN_ACTION  HOTKEY_SOCD_NEUTRAL
+#define HOTKEY_F2_LEFT_MASK    GAMEPAD_MASK_LEFT
+#define HOTKEY_F2_LEFT_ACTION  HOTKEY_SOCD_LAST_INPUT
+#define HOTKEY_F2_RIGHT_MASK   GAMEPAD_MASK_RIGHT
+#define HOTKEY_F2_RIGHT_ACTION HOTKEY_INVERT_Y_AXIS
 
 #endif
